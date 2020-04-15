9fe0984e28679b24da02c865133a47c249000269
Kenny2github <kenny2minecraft@gmail.com>
2020-04-15 14:41:08 +0800

    Add missing BT1 and BT3

---
+++ ProgrammingElective/bt3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void TempSwap(int a, int b);
+void PlusMinusSwap(int a, int b);
+void XORSwap(int a, int b);
+
+int bt3_main(void) {
+    int a,b;
+    printf("Input a and b: \n");
+    scanf("%d %d", &a, &b);
+
+    TempSwap(a,b);      //Swap using a temp variable
+    PlusMinusSwap(a,b); //Swap using + and - operators only
+    XORSwap(a,b);       //Swap using XOR operator only
+
+    return 0;
+}
+
+void TempSwap(int a, int b) {
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("Using 'temp' swap:\t a=%d b=%d \n", a, b);
+}
+
+void PlusMinusSwap(int a, int b) {
+    a += b;
+    b = a - b;
+    a -= b;
+    printf("Using 'Plus-Minus' swap: a=%d b=%d \n", a, b);
+}
+
+void XORSwap(int a, int b) {
+    a ^= b;
+    b ^= a;
+    a ^= b;
+    printf("Using 'XOR' swap:\t a=%d b=%d \n", a, b);
+}
