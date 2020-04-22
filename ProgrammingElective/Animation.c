55c296c353d1f5f68ba43e7616ee70d3c8b427d4
Kenny2github <kenny2minecraft@gmail.com>
2020-04-22 19:21:57 +0800

    never mind about party parrot, can't hardcode. new idea: visualize sine, triangle, saw, square waves

--- ProgrammingElective/Animation.c
+++ ProgrammingElective/Animation.c
@@ -1,197 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <Windows.h>
+#define PERIOD 50
+#define WIDTH 40
+#define HEIGHT 12
+#define HEIGHT2 24
+#define BUFLEN (HEIGHT + 1) * (WIDTH * 2 + 2)
+#define M_PI 3.14159265358979323846
 
-const char* frames[] = {
-"                         .cccc;;cc;';c.\n"
-"                      .,:dkdc:;;:c:,:d:.\n"
-"                     .loc'.,cc::c:::,..;:.\n"
-"                   .cl;....;dkdccc::,...c;\n"
-"                  .c:,';:'..ckc',;::;....;c.\n"
-"                .c:'.,dkkoc:ok:;llllc,,c,';:.\n"
-"               .;c,';okkkkkkkk:;lllll,:kd;.;:,.\n"
-"               co..:kkkkkkkkkk:;llllc':kkc..oNc\n"
-"             .cl;.,oxkkkkkkkkkc,:cll;,okkc'.cO;\n"
-"             ;k:..ckkkkkkkkkkkl..,;,.;xkko:',l'\n"
-"            .,...';dkkkkkkkkkkd;.....ckkkl'.cO;\n"
-"         .,,:,.;oo:ckkkkkkkkkkkdoc;;cdkkkc..cd,\n"
-"      .cclo;,ccdkkl;llccdkkkkkkkkkkkkkkkd,.c;\n"
-"     .lol:;;okkkkkxooc::coodkkkkkkkkkkkko'.oc\n"
-"   .c:'..lkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkd,.oc\n"
-"  .lo;,:cdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkd,.c;\n"
-",dx:..;lllllllllllllllllllllllllllllllllc'...\n"
-"cNO;........................................\n",
-"                .ckx;'........':c.\n"
-"             .,:c:::::oxxocoo::::,',.\n"
-"            .odc'..:lkkoolllllo;..;d,\n"
-"            ;c..:o:..;:..',;'.......;.\n"
-"           ,c..:0Xx::o:.,cllc:,'::,.,c.\n"
-"           ;c;lkXKXXXXl.;lllll;lKXOo;':c.\n"
-"         ,dc.oXXXXXXXXl.,lllll;lXXXXx,c0:\n"
-"         ;Oc.oXXXXXXXXo.':ll:;'oXXXXO;,l'\n"
-"         'l;;kXXXXXXXXd'.'::'..dXXXXO;,l'\n"
-"         'l;:0XXXXXXXX0x:...,:o0XXXXx,:x,\n"
-"         'l;;kXXXXXXXXXKkol;oXXXXXXXO;oNc\n"
-"        ,c'..ckk0XXXXXXXXXX00XXXXXXX0:;o:.\n"
-"      .':;..:do::ooookXXXXXXXXXXXXXXXo..c;\n"
-"    .',',:co0XX0kkkxxOXXXXXXXXXXXXXXXOc..;l.\n"
-"  .:;'..oXXXXXXXXXXXXXXXXXXXXXXXXXXXXXko;';:.\n"
-".ldc..:oOXKXXXXXXKXXKXXXXXXXXXXXXXXXXXXXo..oc\n"
-":0o...:dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxo,.:,\n"
-"cNo........................................;'\n",
-"            .cc;.  ...  .;c.\n"
-"         .,,cc:cc:lxxxl:ccc:;,.\n"
-"        .lo;...lKKklllookl..cO;\n"
-"      .cl;.,:'.okl;..''.;,..';:.\n"
-"     .:o;;dkd,.ll..,cc::,..,'.;:,.\n"
-"     co..lKKKkokl.':lloo;''ol..;dl.\n"
-"   .,c;.,xKKKKKKo.':llll;.'oOxl,.cl,.\n"
-"   cNo..lKKKKKKKo'';llll;;okKKKl..oNc\n"
-"   cNo..lKKKKKKKko;':c:,'lKKKKKo'.oNc\n"
-"   cNo..lKKKKKKKKKl.....'dKKKKKxc,l0:\n"
-"   .c:'.lKKKKKKKKKk;....lKKKKKKo'.oNc\n"
-"     ,:.'oxOKKKKKKKOxxxxOKKKKKKxc,;ol:.\n"
-"     ;c..'':oookKKKKKKKKKKKKKKKKKk:.'clc.\n"
-"   ,xl'.,oxo;'';oxOKKKKKKKKKKKKKKKOxxl:::;,.\n"
-"  .dOc..lKKKkoooookKKKKKKKKKKKKKKKKKKKxl,;ol.\n"
-"  cx,';okKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKl..;lc.\n"
-"  co..:dddddddddddddddddddddddddddddddddl::',::.\n"
-"  co...........................................\n",
-"           .ccccccc.\n"
-"      .,,,;cooolccoo;;,,.\n"
-"     .dOx;..;lllll;..;xOd.\n"
-"   .cdo;',loOXXXXXkll;';odc.\n"
-"  ,ol:;c,':oko:cccccc,...ckl.\n"
-"  ;c.;kXo..::..;c::'.......oc\n"
-",dc..oXX0kk0o.':lll;..cxxc.,ld,\n"
-"kNo.'oXXXXXXo',:lll;..oXXOo;cOd.\n"
-"KOc;oOXXXXXXo.':lol;..dXXXXl';xc\n"
-"Ol,:k0XXXXXX0c.,clc'.:0XXXXx,.oc\n"
-"KOc;dOXXXXXXXl..';'..lXXXXXo..oc\n"
-"dNo..oXXXXXXXOx:..'lxOXXXXXk,.:; ..\n"
-"cNo..lXXXXXXXXXOolkXXXXXXXXXkl,..;:';.\n"
-".,;'.,dkkkkk0XXXXXXXXXXXXXXXXXOxxl;,;,;l:.\n"
-"  ;c.;:''''':doOXXXXXXXXXXXXXXXXXXOdo;';clc.\n"
-"  ;c.lOdood:'''oXXXXXXXXXXXXXXXXXXXXXk,..;ol.\n"
-"  ';.:xxxxxocccoxxxxxxxxxxxxxxxxxxxxxxl::'.';;.\n"
-"  ';........................................;l'\n",
-"\n"
-"        .;:;;,.,;;::,.\n"
-"     .;':;........'co:.\n"
-"   .clc;'':cllllc::,.':c.\n"
-"  .lo;;o:coxdllllllc;''::,,.\n"
-".c:'.,cl,.'l:',,;;'......cO;\n"
-"do;';oxoc;:l;;llllc'.';;'.,;.\n"
-"c..ckkkkkkkd,;llllc'.:kkd;.':c.\n"
-"'.,okkkkkkkkc;lllll,.:kkkdl,cO;\n"
-"..;xkkkkkkkkc,ccll:,;okkkkk:,co,\n"
-"..,dkkkkkkkkc..,;,'ckkkkkkkc;ll.\n"
-"..'okkkkkkkko,....'okkkkkkkc,:c.\n"
-"c..ckkkkkkkkkdl;,:okkkkkkkkd,.',';.\n"
-"d..':lxkkkkkkkkxxkkkkkkkkkkkdoc;,;'..'.,.\n"
-"o...'';llllldkkkkkkkkkkkkkkkkkkdll;..'cdo.\n"
-"o..,l;'''''';dkkkkkkkkkkkkkkkkkkkkdlc,..;lc.\n"
-"o..;lc;;;;;;,,;clllllllllllllllllllllc'..,:c.\n"
-"o..........................................;'\n",
-"\n"
-"           .,,,,,,,,,.\n"
-"         .ckKxodooxOOdcc.\n"
-"      .cclooc'....';;cool.\n"
-"     .loc;;;;clllllc;;;;;:;,.\n"
-"   .c:'.,okd;;cdo:::::cl,..oc\n"
-"  .:o;';okkx;';;,';::;'....,:,.\n"
-"  co..ckkkkkddkc,cclll;.,c:,:o:.\n"
-"  co..ckkkkkkkk:,cllll;.:kkd,.':c.\n"
-".,:;.,okkkkkkkk:,cclll;.ckkkdl;;o:.\n"
-"cNo..ckkkkkkkkko,.;loc,.ckkkkkc..oc\n"
-",dd;.:kkkkkkkkkx;..;:,.'lkkkkko,.:,\n"
-"  ;:.ckkkkkkkkkkc.....;ldkkkkkk:.,'\n"
-",dc..'okkkkkkkkkxoc;;cxkkkkkkkkc..,;,.\n"
-"kNo..':lllllldkkkkkkkkkkkkkkkkkdcc,.;l.\n"
-"KOc,c;''''''';lldkkkkkkkkkkkkkkkkkc..;lc.\n"
-"xx:':;;;;,.,,...,;;cllllllllllllllc;'.;od,\n"
-"cNo.....................................oc\n",
-"\n"
-"\n"
-"                   .ccccccc.\n"
-"               .ccckNKOOOOkdcc.\n"
-"            .;;cc:ccccccc:,:c::,,.\n"
-"         .c;:;.,cccllxOOOxlllc,;ol.\n"
-"        .lkc,coxo:;oOOxooooooo;..:,\n"
-"      .cdc.,dOOOc..cOd,.',,;'....':l.\n"
-"      cNx'.lOOOOxlldOc..;lll;.....cO;\n"
-"     ,do;,:dOOOOOOOOOl'':lll;..:d:''c,\n"
-"     co..lOOOOOOOOOOOl'':lll;.'lOd,.cd.\n"
-"     co.'dOOOOOOOOOOOo,.;llc,.,dOOc..dc\n"
-"     co..lOOOOOOOOOOOOc.';:,..cOOOl..oc\n"
-"   .,:;.'::lxOOOOOOOOOo:'...,:oOOOc.'dc\n"
-"   ;Oc..cl'':lldOOOOOOOOdcclxOOOOx,.cd.\n"
-"  .:;';lxl''''':lldOOOOOOOOOOOOOOc..oc\n"
-",dl,.'cooc:::,....,::coooooooooooc'.c:\n"
-"cNo.................................oc\n",
-"\n"
-"\n"
-"\n"
-"                        .cccccccc.\n"
-"                  .,,,;;cc:cccccc:;;,.\n"
-"                .cdxo;..,::cccc::,..;l.\n"
-"               ,do:,,:c:coxxdllll:;,';:,.\n"
-"             .cl;.,oxxc'.,cc,.';;;'...oNc\n"
-"             ;Oc..cxxxc'.,c;..;lll;...cO;\n"
-"           .;;',:ldxxxdoldxc..;lll:'...'c,\n"
-"           ;c..cxxxxkxxkxxxc'.;lll:'','.cdc.\n"
-"         .c;.;odxxxxxxxxxxxd;.,cll;.,l:.'dNc\n"
-"        .:,''ccoxkxxkxxxxxxx:..,:;'.:xc..oNc\n"
-"      .lc,.'lc':dxxxkxxxxxxxol,...',lx:..dNc\n"
-"     .:,',coxoc;;ccccoxxxxxxxxo:::oxxo,.cdc.\n"
-"  .;':;.'oxxxxxc''''';cccoxxxxxxxxxxxc..oc\n"
-",do:'..,:llllll:;;;;;;,..,;:lllllllll;..oc\n"
-"cNo.....................................oc\n",
-"\n"
-"\n"
-"                              .ccccc.\n"
-"                         .cc;'coooxkl;.\n"
-"                     .:c:::c:,,,,,;c;;,.'.\n"
-"                   .clc,',:,..:xxocc;'..c;\n"
-"                  .c:,';:ox:..:c,,,,,,...cd,\n"
-"                .c:'.,oxxxxl::l:.,loll;..;ol.\n"
-"                ;Oc..:xxxxxxxxx:.,llll,....oc\n"
-"             .,;,',:loxxxxxxxxx:.,llll;.,,.'ld,\n"
-"            .lo;..:xxxxxxxxxxxx:.'cllc,.:l:'cO;\n"
-"           .:;...'cxxxxxxxxxxxxoc;,::,..cdl;;l'\n"
-"         .cl;':,'';oxxxxxxdxxxxxx:....,cooc,cO;\n"
-"     .,,,::;,lxoc:,,:lxxxxxxxxxxxo:,,;lxxl;'oNc\n"
-"   .cdxo;':lxxxxxxc'';cccccoxxxxxxxxxxxxo,.;lc.\n"
-"  .loc'.'lxxxxxxxxocc;''''';ccoxxxxxxxxx:..oc\n"
-"olc,..',:cccccccccccc:;;;;;;;;:ccccccccc,.'c,\n"
-"Ol;......................................;l'\n",
-"\n"
-"                              ,ddoodd,\n"
-"                         .cc' ,ooccoo,'cc.\n"
-"                      .ccldo;...',,...;oxdc.\n"
-"                   .,,:cc;.,'..;lol;;,'..lkl.\n"
-"                  .dOc';:ccl;..;dl,.''.....oc\n"
-"                .,lc',cdddddlccld;.,;c::'..,cc:.\n"
-"                cNo..:ddddddddddd;':clll;,c,';xc\n"
-"               .lo;,clddddddddddd;':clll;:kc..;'\n"
-"             .,c;..:ddddddddddddd:';clll,;ll,..\n"
-"             ;Oc..';:ldddddddddddl,.,c:;';dd;..\n"
-"           .''',:c:,'cdddddddddddo:,''..'cdd;..\n"
-"         .cdc';lddd:';lddddddddddddd;.';lddl,..\n"
-"      .,;::;,cdddddol;;lllllodddddddlcldddd:.'l;\n"
-"     .dOc..,lddddddddlcc:;'';cclddddddddddd;;ll.\n"
-"   .coc,;::ldddddddddddddlcccc:ldddddddddl:,cO;\n"
-",xl::,..,cccccccccccccccccccccccccccccccc:;':xx,\n"
-"cNd.........................................;lOc\n"
-};
+void frame(int offset);
 
 int anime_main() {
-	register int i = 0;
+	int i = 0;
 	while (1) {
 		system("cls");
-		printf(frames[i]);
-		i = (i + 1) % 10;
-		Sleep(30);
+		frame(++i);
 	}
 	return 0;
+}
+
+void frame(int offset) {
+	register int i, j;
+	char buffer[BUFLEN+1]; // avoid many small printf calls
+	int pos = -1; // prefer ++pos over pos++ because pos++ has to remember old value
+	printf("%-40s| Triangle\n", "Sine"); // width of -40 means align left to 40 chars
+	for (i = HEIGHT/2; i >= -HEIGHT/2; --i) {
+		// sine wave
+		for (j = 0; j < WIDTH; ++j) {
+			if (round(sin(2 * M_PI * ((double)j + offset) / PERIOD) * HEIGHT/2) == i) {
+				buffer[++pos] = '*'; // write plot into buffer
+			} else {
+				buffer[++pos] = ' ';
+			}
+		}
+		buffer[++pos] = '|'; // separator between two adjacent graphs
+		// triangle wave
+		for (j = 0; j < WIDTH; ++j) {
+			// function for triangle wave starting at 0
+			if (abs((j + offset) % HEIGHT2 - HEIGHT2 / 2) - HEIGHT2/4 == i) {
+				buffer[++pos] = '*';
+			} else {
+				buffer[++pos] = ' ';
+			}
+		}
+		buffer[++pos] = '\n'; // one y-level done
+	}
+	buffer[BUFLEN] = 0; // NUL-terminate string
+	puts(buffer);
+	pos = -1;
+	printf("%-40s| Square\n", "Sawtooth");
+	for (i = HEIGHT/2; i >= -HEIGHT/2; --i) {
+		// sawtooth wave
+		for (j = 0; j < WIDTH; ++j) {
+			if ((j + offset) % HEIGHT == 0) { // draw vertical line at cycle start
+				buffer[++pos] = '*';
+			} else if ((j + offset) % HEIGHT == (i+HEIGHT/2)) { // function for sawtooth
+				buffer[++pos] = '*';
+			} else {
+				buffer[++pos] = ' ';
+			}
+		}
+		buffer[++pos] = '|';
+		// square wave
+		for (j = 0; j < WIDTH; ++j) {
+			if ((j + offset) % (PERIOD/2) == 0) { // ditto with previous
+				buffer[++pos] = '*';
+			} else if (round(
+				sin(2 * M_PI *((double)j + offset) / PERIOD)
+				/ fabs(sin(2 * M_PI *((double)j + offset) / PERIOD))
+			) * HEIGHT/2 == i) { // sqr(x) = sgn(sin(x)) = sin(x)/abs(sin(x))
+				buffer[++pos] = '*';
+			} else {
+				buffer[++pos] = ' ';
+			}
+		}
+		buffer[++pos] = '\n';
+	}
+	buffer[BUFLEN] = 0; // NUL-terminate string
+	puts(buffer);
 }
